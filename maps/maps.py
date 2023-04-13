a7a2a8660a72879ce869d9a6589287a500225111
Kazakov Kostya <leap74rus@gmail.com>
2023-04-13 08:06:39 +0300

    exercise rating answer no map

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,16 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+        
+        films = 0
+        rating_kinopoisk_all = 0
+        for movie in list_of_movies:
+            if len(movie["country"].split(",")) >= 2:
+                if movie["rating_kinopoisk"] != "":
+                    if float(movie["rating_kinopoisk"]) != 0.0:
+                        films += 1
+                        rating_kinopoisk_all += float(movie["rating_kinopoisk"]) 
+        return rating_kinopoisk_all / films
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
